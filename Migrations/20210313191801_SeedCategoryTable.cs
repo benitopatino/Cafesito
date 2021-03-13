7a8943f42da17351a0e53f69d8a3307a9e713ac1
Benito <contact@benitopatino.com>
2021-03-13 11:23:03 -0800

    Created migration to seed Category table:

---
+++ Migrations/20210313191801_SeedCategoryTable.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cafesito.Migrations
+{
+    public partial class SeedCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Category (Id, Name) VALUES ('1', 'Coffee')");
+            migrationBuilder.Sql("INSERT INTO Category (Id, Name) VALUES ('2', 'Tea')");
+            migrationBuilder.Sql("INSERT INTO Category (Id, Name) VALUES ('3', 'Merchandise')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

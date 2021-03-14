dc56d0f1d1bddf0e22933a8b9e3f1a666c8a12ab
Benito <contact@benitopatino.com>
2021-03-13 20:49:42 -0800

    Seeded OrderStatus table

---
+++ Migrations/20210314044402_SeedOrderStatusTable.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cafesito.Migrations
+{
+    public partial class SeedOrderStatusTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO OrderStatus (Id, Name) VALUES ('1', 'In Progress')");
+            migrationBuilder.Sql("INSERT INTO OrderStatus (Id, Name) VALUES ('2', 'Shipped')");
+            migrationBuilder.Sql("INSERT INTO OrderStatus (Id, Name) VALUES ('3', 'Delivered')");
+            migrationBuilder.Sql("INSERT INTO OrderStatus (Id, Name) VALUES ('4', 'Cancelled')");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
